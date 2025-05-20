e32f1600765ba83b529d0270ead892c1dd3b1e8b
Miri Radunsky <miriradunsky@gmail.com>
2025-05-20 22:26:49 +0300

    db

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
-export class myDB {
+export class myDB{
     static DB: myDB = new myDB();
-    DB_NAME = 'Shop_DB';
+    DB_NAME = 'Shop_DB';    
     URI = `mongodb://localhost:27017/${this.DB_NAME}`;
 
     async connectToDb(): Promise<void> {
@@ -11,12 +11,13 @@ export class myDB {
             console.log('Connected to MongoDB (Mongoose)');
         } catch (err) {
             console.error('MongoDB connection error:', err);
-            process.exit(1);
+        process.exit(1);
         }
     }
 
-    static async getDB(): Promise<myDB> {
-        if (mongoose.connection.readyState === 0)
+    static async getDB(): Promise<myDB>
+    {
+        if( mongoose.connection.readyState === 0)
             await this.DB.connectToDb();
         return this.DB;
     }
